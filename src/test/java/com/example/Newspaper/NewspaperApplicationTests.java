5ae2da9182eaef1ce082e04ca5edd5a7b724653e
unknown <fahima.alhabsi@gmail.com>
2023-05-22 10:26:09 +0400

    Initial commit

---
+++ src/test/java/com/example/Newspaper/NewspaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Newspaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspaperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
